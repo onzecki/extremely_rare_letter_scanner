8a4b6ebfc0049c20cf92bd6cf33ff1a59da2a812
onzecki <30354910+Honzecki@users.noreply.github.com>
2023-11-28 23:31:25 +0100

    add: intro text

--- extremely_rare_letter_scanner/src/main.rs
+++ extremely_rare_letter_scanner/src/main.rs
@@ -3,6 +3,7 @@ use std::io;
 use colored::Colorize;
 
 fn main() {
+    println!("▶ {} {}{}{}", "Type".bright_blue().bold(), "anything,".purple().bold(),  " and ERLS will check whether your text contains ".bright_blue().bold(), "rare two-letter combinations!".purple().bold());
     let rare_combinations = fs::read_to_string("rare.txt").unwrap_or("bk fq jc jt mj qh qx vj zh bq fv jd jv mq qj vk xb zj bx fx jf jw mx vm xg zn cb fz jg jx mz ql sx vn xj zq cf gq jh jy pq qm sz vp xk zr cg gv jk jz pv qn tq vq xv zs cj gx jl kq px qo tx vt xz zx cp hk jm kv qb qp vb vw yq cv hv jn kx qc qr vc vx yv cw hx jp kz qd qs vd vz yz cx hz lq qe qt vf zb dx iy jr lx qf qv vg wv zc fk jb js mg qw vh wx zg".to_string());
     let ultra_rare_combinations =
         fs::read_to_string("ultra_rare.txt").unwrap_or("jq qg qk qy qz wq wz".to_string());
